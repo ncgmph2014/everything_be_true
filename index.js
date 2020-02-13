ba4d9b8b31d57d99f25a4a565312859eb7663e17
ncgmph2014 <nicole.gathany@gmail.com>
2020-02-13 11:39:12 -0500

    added examples

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-function truthCheck(collection, pre) {
+function console.log(truthCheck(collection, pre) {
   // Is everyone being true?
   return pre;
 }
 
-truthCheck(
+console.log(truthCheck(
   [
     { user: "Tinky-Winky", sex: "male" },
     { user: "Dipsy", sex: "male" },
@@ -12,3 +12,15 @@ truthCheck(
   ],
   "sex"
 );
+
+
+console.log(truthCheck([{"user": "Tinky-Winky", "sex": "male"}, {"user": "Dipsy", "sex": "male"}, {"user": "Laa-Laa", "sex": "female"}, {"user": "Po", "sex": "female"}], "sex") + "should return true.")
+console.log(truthCheck([{"user": "Tinky-Winky", "sex": "male"}, {"user": "Dipsy"}, {"user": "Laa-Laa", "sex": "female"}, {"user": "Po", "sex": "female"}], "sex")  + "should return false.")
+console.log(truthCheck([{"user": "Tinky-Winky", "sex": "male", "age": 0}, {"user": "Dipsy", "sex": "male", "age": 3}, {"user": "Laa-Laa", "sex": "female", "age": 5}, {"user": "Po", "sex": "female", "age": 4}], "age") + "should return false.")
+console.log(truthCheck([{"name": "Pete", "onBoat": true}, {"name": "Repeat", "onBoat": true}, {"name": "FastForward", "onBoat": null}], "onBoat") + "should return false")
+console.log(truthCheck([{"name": "Pete", "onBoat": true}, {"name": "Repeat", "onBoat": true, "alias": "Repete"}, {"name": "FastForward", "onBoat": true}], "onBoat") + "should return true")
+console.log(truthCheck([{"single": "yes"}], "single") + "should return true")
+console.log(truthCheck([{"single": ""}, {"single": "double"}], "single") + "should return false")
+console.log(truthCheck([{"single": "double"}, {"single": undefined}], "single") + 
+"should return false")
+console.log(truthCheck([{"single": "double"}, {"single": NaN}], "single") + "should return false")
